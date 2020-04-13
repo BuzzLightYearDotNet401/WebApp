c99a9a720a25a6bec38e00f1969e1a75f37a85c5
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-04-13 14:14:55 -0700

    Added static files capability to app.

--- HealthAtHome/HealthAtHome/Startup.cs
+++ HealthAtHome/HealthAtHome/Startup.cs
@@ -27,6 +27,8 @@ namespace HealthAtHome
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
