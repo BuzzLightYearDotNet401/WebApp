01b3a6f7f6b26d6c403ae6be43da01587f9e4c3a
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-04-14 17:03:24 -0700

    Added Routine Controller.

---
+++ HealthAtHome/HealthAtHome/Controllers/RoutineController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HealthAtHome.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthAtHome.Controllers
+{
+    [Route("[controller]")]
+    public class RoutineController : Controller
+    {
+        private readonly IExercise _exercise;
+
+        public RoutineController(IExercise exercise)
+        {
+            _exercise = exercise;
+        }
+
+        // Get all Routines View.
+        // Route: /routine
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var result = await _exercise.GetAllExercises();
+
+            return View(result);
+        }
+
+        // Route: /routine/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetARoutine(int id)
+        {
+            var result = await _exercise.GetExerciseByID(id);
+
+            return View(result);
+        }
+    }
+}
